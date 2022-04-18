2cc0f9bf7b5537825c09f73b01c8c4a72b3f927d
jabin34 <isratjabinesha@gmail.com>
2022-04-18 20:32:30 +0900

    Review section done

--- homefoodi/src/components/CustomerReview/CustomerReview.js
+++ homefoodi/src/components/CustomerReview/CustomerReview.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const CustomerReview = () => {
+    const [reviews,setReviews] = useState([]);
+    useEffect(()=>{
+      fetch('review.json')
+      .then(res=>res.json())
+      .then(data =>setReviews(data));
+    },[])
     return (
-        <div className='container'>
-            <div>
-                
+        <div className='container mt-2'>
+            <h3 className='text-center'> My Clients Review</h3>
+            <div className='d-flex p-3 mx-auto flex-md-row flex-sm-column flex-column justify-content-around'>
+            {reviews.map(review=>(
+                <div class="card m-2" style={{}}>
+                <div class="card-body">
+                  
+                  
+                  <p class="card-text">{review.reviews}</p>
+                  <h5 class="card-title">Rating: {review.rating}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">{review.time}</h6>
+            
+                </div>
+              </div>
+            ))}
             </div>
         </div>
     );
--- homefoodi/src/components/Home/Home.js
+++ homefoodi/src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import Banner from '../Bannner/Banner';
+import CustomerReview from '../CustomerReview/CustomerReview';
 import Services from '../Service/Services';
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
         <div className='container-fluid p-0'>
             <Banner/>
             <Services/>
+            <CustomerReview/>
             <Footer/>
         </div>
     );
